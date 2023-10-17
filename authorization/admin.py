a7ce289ed05d95b0aaf25b55b5760458db94bba8
Ksenia Panova <rybinak95@gmail.com>
2023-10-17 12:42:00 +0300

    Adding authorization

--- authorization/admin.py
+++ authorization/admin.py
@@ -1,4 +1,1 @@
-from django.contrib import admin
-from authorization.models import Account
 
-admin.site.register(Account)
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import View
+from users.models import User
 
 
-class TasksView(TemplateView):
-    template_name = 'tasks.html'
+class TasksView(View):
+    def get(self, request):
+        if request.method == 'GET':
+            user_id = request.session['user_id']
+            user = User.objects.get(id=user_id)
+            return render(
+                request,
+                'tasks.html',
+                context={'user': user}
+            )
