01a392c8093e5bf842e2052a6b05fce3e54a238d
Ksenia Panova <rybinak95@gmail.com>
2023-10-24 12:01:34 +0300

    Header's creation

--- tasks/views.py
+++ tasks/views.py
@@ -6,10 +6,10 @@ from users.models import User
 class TasksView(View):
     def get(self, request):
         if request.method == 'GET':
-            user_id = request.session['user_id']
-            user = User.objects.get(id=user_id)
+            session_user_id = request.session['user_id']
+            user = User.objects.get(id=session_user_id)
             return render(
                 request,
                 'tasks.html',
-                context={'user': user}
+                context={'user': user, 'menu_user_id': session_user_id}
             )
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:user_id>', views.ProfileView.as_view(), name='profile')
+    path('<int:url_user_id>', views.ProfileView.as_view(), name='profile')
 ]
