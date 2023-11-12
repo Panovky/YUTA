47975788a1d48701ee6431677bea2f47fe80be6b
Ksenia Panova <rybinak95@gmail.com>
2023-11-13 00:46:31 +0300

    Add teams API (method get)

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('authorization', views.AuthorizationView.as_view(), name="authorization"),
-    path('profile', views.ProfileView.as_view(), name="profile")
+    path('profile', views.ProfileView.as_view(), name="profile"),
+    path('teams', views.TeamsView.as_view(), name="teams")
 ]
--- api/views.py
+++ api/views.py
@@ -44,3 +44,17 @@ class ProfileView(APIView):
         }
 
         return JsonResponse(data=response_data)
+
+
+class TeamsView(APIView):
+    def get(self, request):
+        user_id = request.data.get('user_id')
+        managed_teams = User.objects.get(id=user_id).leader_teams.all()
+        others_teams = User.objects.get(id=user_id).teams.all()
+
+        response_data = {
+            'managed_teams': [team.serialize_for_teams_view() for team in managed_teams],
+            'others_teams': [team.serialize_for_teams_view() for team in others_teams],
+        }
+
+        return JsonResponse(data=response_data)
