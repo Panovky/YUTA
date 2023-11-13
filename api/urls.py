a8c2ca92a5abc2454eff51128051f7e8de81b927
Ksenia Panova <rybinak95@gmail.com>
2023-11-13 13:46:40 +0300

    Fix bug

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('authorization', views.AuthorizationView.as_view(), name="authorization"),
-    path('profile', views.ProfileView.as_view(), name="profile"),
-    path('teams', views.TeamsView.as_view(), name="teams")
+    path('authorization', views.AuthorizationView.as_view()),
+    path('profile', views.ProfileView.as_view()),
+    path('teams', views.TeamsView.as_view())
 ]
