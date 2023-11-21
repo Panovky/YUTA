9b454b0879b4dc6e6292126e80bb1c4444fe8cff
Ksenia Panova <rybinak95@gmail.com>
2023-11-21 19:12:33 +0300

    Corrections in teams

--- teams/urls.py
+++ teams/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new/<str:stage>', views.NewTeamView.as_view(), name='new_team'),
     path('', views.TeamsView.as_view(), name='teams'),
 ]
