26a0c3bfb79acac16bd1a724b4e8cc2ee791c103
Ksenia Panova <rybinak95@gmail.com>
2024-02-27 15:56:14 +0300

    Added APIView to get all projects

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('authorization', views.AuthorizationView.as_view()),
     path('profile', views.ProfileView.as_view()),
+    path('projects', views.ProjectsView.as_view()),
+    path('tasks', views.TasksView.as_view()),
     path('teams', views.TeamsView.as_view())
 ]
